a332017de6b326b4636f6f738bb22bbb35fb9414
Radek Wierzbicki <radekw@me.com>
2024-02-20 23:03:25 -0500

    more lazy

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        null_ls.builtins.formatting.terraform_fmt,
+        null_ls.builtins.diagnostics.terraform_validate,
+      })
+    end,
+  },
+}
